179fa4bfb52bb80283a6d4a734fde60012bf84db
Gabriel Guarisa <gabrielguarisa@gmail.com>
2023-01-05 19:52:18 -0300

    Add constant and list models

--- retack/models/__init__.py
+++ retack/models/__init__.py
@@ -1,3 +1,4 @@
+from retack.models.constants import ConstantModel, ListModel
 from retack.models.inputs import InputModel
 from retack.models.start import StartModel
 from retack.utils.model_registry import ModelRegistry
@@ -6,5 +7,7 @@ model_registry = ModelRegistry()
 
 model_registry.register("Input", InputModel)
 model_registry.register("Start", StartModel)
+model_registry.register("Constant", ConstantModel)
+model_registry.register("List", ListModel)
 
 __all__ = ["model_registry"]
---
+++ retack/models/constants.py
@@ -0,0 +1,35 @@
+import typing
+
+import pydantic
+
+from retack.models.base import InputConnectionModel, OutputConnectionModel
+
+
+class ConstantMetadataModel(pydantic.BaseModel):
+    value: str
+
+
+class ConstantInputsModel(pydantic.BaseModel):
+    input_void: typing.Optional[InputConnectionModel] = None
+
+
+class ConstantOutputsModel(pydantic.BaseModel):
+    output_0: OutputConnectionModel
+
+
+class ConstantModel(pydantic.BaseModel):
+    id: str
+    data: ConstantMetadataModel
+    inputs: typing.Optional[ConstantInputsModel] = None
+    outputs: ConstantOutputsModel
+
+
+class ListMetadataModel(pydantic.BaseModel):
+    value: typing.List[str]
+
+
+class ListModel(pydantic.BaseModel):
+    id: str
+    data: ListMetadataModel
+    inputs: typing.Optional[ConstantInputsModel] = None
+    outputs: ConstantOutputsModel
