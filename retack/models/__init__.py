da1958c0a0b7718afcae929c88d2bbe6b8cefcb7
Gabriel Guarisa <gabrielguarisa@gmail.com>
2023-01-06 15:16:25 -0300

    Add check model

--- retack/models/__init__.py
+++ retack/models/__init__.py
@@ -1,3 +1,4 @@
+from retack.models.check import CheckModel
 from retack.models.constants import ConstantModel, ListModel
 from retack.models.inputs import InputModel
 from retack.models.outputs import BoolOutputModel
@@ -11,5 +12,6 @@ model_registry.register("Start", StartModel)
 model_registry.register("Constant", ConstantModel)
 model_registry.register("List", ListModel)
 model_registry.register("BoolOutput", BoolOutputModel)
+model_registry.register("Check", CheckModel)
 
 __all__ = ["model_registry"]
