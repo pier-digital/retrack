f57ec26358f9c1cba5ffa1e236a6982dd0c8261f
lunfelix <luananfelix26@gmail.com>
2023-01-10 20:42:21 -0300

    Formatting Logic Test

--- tests/test_nodes/test_logic.py
+++ tests/test_nodes/test_logic.py
@@ -3,69 +3,70 @@ import pandas as pd
 from retack.nodes.logic import And, Or, Not
 
 input_data_And = {
-        "id": 15,
-		"data": {},
-		"inputs": {
-			"input_bool_0": {
-				"connections": []
-			},
-			"input_bool_1": {
-				"connections": []
-			}
+    "id": 15,
+	"data": {},
+	"inputs": {
+		"input_bool_0": {
+			"connections": []
 		},
-		"outputs": {
-			"output_bool": {
-				"connections": []
-			}
-		},
-		"position": [
-			600.2264533807651,
-			1113.094677616452
-		],
-		"name": "And"
-    }
+		"input_bool_1": {
+			"connections": []
+		}
+	},
+	"outputs": {
+		"output_bool": {
+			"connections": []
+		}
+	},
+	"position": [
+		600.2264533807651,
+		1113.094677616452
+	],
+	"name": "And"
+}
+
 input_data_Or = {
-        "id": 16,
-		"data": {},
-		"inputs": {
-			"input_bool_0": {
-				"connections": []
-			},
-			"input_bool_1": {
-				"connections": []
-			}
-		},
-		"outputs": {
-			"output_bool": {
-				"connections": []
-			}
-		},
-		"position": [
-			891.9762574619829,
-			1017.9278275583388
-		],
-		"name": "Or"
-    }
+    "id": 16,
+	"data": {},
+	"inputs": {
+		"input_bool_0": {
+			"connections": []
+	},
+		"input_bool_1": {
+			"connections": []
+		}
+	},
+	"outputs": {
+		"output_bool": {
+			"connections": []
+		}
+	},
+	"position": [
+		891.9762574619829,
+		1017.9278275583388
+	],
+	"name": "Or"
+}
 
 input_data_Not = {
-        "id": 17,
-		"data": {},
-		"inputs": {
-			"input_bool": {
-				"connections": []
-			}
-		},
-		"outputs": {
-			"output_bool": {
+    "id": 17,
+	"data": {},
+	"inputs": {
+		"input_bool": {
 			"connections": []
-			}
-		},
-		"position": [
-			1195.5017776430686,
-			1080.1383409685295
-		],
-		"name": "Not"
-    }
+		}
+	},
+	"outputs": {
+		"output_bool": {
+		"connections": []
+		}
+	},
+	"position": [
+		1195.5017776430686,
+		1080.1383409685295
+	],
+	"name": "Not"
+}
 
 def test_Logic_node():
     And_node = And(**input_data_And)
