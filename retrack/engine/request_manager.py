33e3d507dcb9bf872b68ba1f3266b604522c1139
Gabriel Guarisa <gabrielguarisa@gmail.com>
2023-02-17 18:17:34 -0300

    Remove request manager dict args

--- retrack/engine/request_manager.py
+++ retrack/engine/request_manager.py
@@ -2,20 +2,20 @@ import typing
 
 import pydantic
 
-from retrack.nodes.inputs import Input
+from retrack.nodes.base import BaseNode, NodeKind
 
 
 class RequestManager:
-    def __init__(self, inputs: typing.List[Input]):
+    def __init__(self, inputs: typing.List[BaseNode]):
         self._model = None
         self.inputs = inputs
 
     @property
-    def inputs(self) -> typing.List[Input]:
+    def inputs(self) -> typing.List[BaseNode]:
         return self._inputs
 
     @inputs.setter
-    def inputs(self, inputs: typing.List[Input]):
+    def inputs(self, inputs: typing.List[BaseNode]):
         if not isinstance(inputs, list):
             raise TypeError(f"inputs must be a list, not {type(inputs)}")
 
@@ -23,11 +23,14 @@ class RequestManager:
         formated_inputs = []
 
         for i in range(len(inputs)):
-            if isinstance(inputs[i], dict):
-                inputs[i] = Input(**inputs[i])
-            elif not isinstance(inputs[i], Input):
+            if not isinstance(inputs[i], BaseNode):
                 raise TypeError(
                     f"inputs[{i}] must be a dict or an InputModel, not {type(inputs[i])}"
+                )
+
+            if inputs[i].kind() != NodeKind.INPUT:
+                raise TypeError(
+                    f"inputs[{i}] must be an InputModel, not {type(inputs[i])}"
                 )
 
             if inputs[i].data.name not in input_names:
@@ -37,13 +40,25 @@ class RequestManager:
         self._inputs = formated_inputs
 
         if len(self.inputs) > 0:
-            self._model = self._create_model()
+            self._model = self.__create_model()
+        else:
+            self._model = None
 
     @property
     def model(self) -> typing.Type[pydantic.BaseModel]:
         return self._model
 
-    def _create_model(self) -> typing.Type[pydantic.BaseModel]:
+    def __create_model(
+        self, model_name: str = "RequestModel"
+    ) -> typing.Type[pydantic.BaseModel]:
+        """Create a pydantic model from the RequestManager's inputs
+
+        Args:
+            model_name (str, optional): The name of the model. Defaults to "RequestModel".
+
+        Returns:
+            typing.Type[pydantic.BaseModel]: The pydantic model
+        """
         fields = {}
         for input_field in self.inputs:
             fields[input_field.data.name] = (
@@ -53,7 +68,7 @@ class RequestManager:
             )
 
         return pydantic.create_model(
-            "RequestModel",
+            model_name,
             **fields,
         )
 
@@ -63,6 +78,17 @@ class RequestManager:
             typing.Dict[str, str], typing.List[typing.Dict[str, str]]
         ],
     ) -> typing.List[pydantic.BaseModel]:
+        """Validate the payload against the RequestManager's model
+
+        Args:
+            payload (typing.Union[typing.Dict[str, str], typing.List[typing.Dict[str, str]]]): The payload to validate
+
+        Raises:
+            ValueError: If the RequestManager has no model
+
+        Returns:
+            typing.List[pydantic.BaseModel]: The validated payload
+        """
         if self.model is None:
             raise ValueError("No inputs found")
 
