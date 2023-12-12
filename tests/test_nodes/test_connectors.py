72a2173d836b4ad9df68fc47d0326a1e048d1c2b
Gabriel Guarisa <gabrielguarisa@gmail.com>
2023-12-12 16:26:25 -0300

    Update version and add ConnectorMetadataModel

--- tests/test_nodes/test_connectors.py
+++ tests/test_nodes/test_connectors.py
@@ -7,7 +7,12 @@ from retrack.nodes.connectors import BaseConnector
 def connector_dict():
     return {
         "id": 1,
-        "data": {"name": "example", "default": "Hello World"},
+        "data": {
+            "name": "example_name",
+            "default": "Hello World",
+            "service": "example_service",
+            "identifier": "example_identifier",
+        },
         "inputs": {},
         "outputs": {
             "output_value": {
@@ -24,6 +29,11 @@ def test_create_base_connector(connector_dict):
 
     assert isinstance(connector, BaseConnector)
     assert connector.kind() == "connector"
+
+    assert connector.data.name == "example_name"
+    assert connector.data.default == "Hello World"
+    assert connector.data.service == "example_service"
+    assert connector.data.identifier == "example_identifier"
 
     with pytest.raises(NotImplementedError):
         _ = connector.generate_input_nodes()
