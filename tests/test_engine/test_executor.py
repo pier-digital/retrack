40b804a5276ce5c6df0de277b62183722bb6b7a9
Gabriel Guarisa <gabrielguarisa@gmail.com>
2024-06-17 11:02:09 -0300

    Add new test case

--- tests/test_engine/test_executor.py
+++ tests/test_engine/test_executor.py
@@ -210,21 +210,6 @@ def test_flows(filename, in_values, expected_out_values):
                 {"output": 4.0, "message": None},
             ],
         ),
-        (
-            "rule-of-rules-with-connector",
-            [
-                {"prediction": "1", "var": "1"},
-                {"prediction": "2", "var": "1"},
-                {"prediction": "3", "var": "1"},
-                {"prediction": "4", "var": "1"},
-            ],
-            [
-                {"output": 1.0, "message": None},
-                {"output": 2.0, "message": None},
-                {"output": 3.0, "message": None},
-                {"output": 4.0, "message": None},
-            ],
-        ),
     ],
 )
 def test_create_from_json(filename, in_values, expected_out_values):
@@ -233,6 +218,11 @@ def test_create_from_json(filename, in_values, expected_out_values):
 
     assert isinstance(out_values, pd.DataFrame)
     assert out_values.to_dict(orient="records") == expected_out_values
+
+
+def test_create_invalid_rule_of_rules_with_connector():
+    with pytest.raises(ValueError):
+        from_json("tests/resources/invalid-rule-of-rules-with-connector.json")
 
 
 def test_create_from_json_with_invalid_type():
