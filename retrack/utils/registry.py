b7db3e52a1fca04a48846484e446782577ada637
Gabriel Guarisa <gabrielguarisa@gmail.com>
2024-09-03 11:23:06 -0300

    Refactor registry class to follow key/value nomenclature

--- retrack/utils/registry.py
+++ retrack/utils/registry.py
@@ -1,48 +1,68 @@
 import typing
+import warnings
 
 
 class Registry:
-    """A simple registry for storing data."""
+    """A simple registry for storing value."""
 
     def __init__(self, case_sensitive: bool = False):
-        self._data = {}
+        self._memory = {}
         self._case_sensitive = case_sensitive
 
-    def register(self, name: str, data: typing.Any, overwrite: bool = False):
+    def register(self, key: str, value: typing.Any, overwrite: bool = False):
         """Register an entry."""
         if not self._case_sensitive:
-            name = name.lower()
+            key = key.lower()
 
-        if name in self._data and not overwrite:
-            raise ValueError(f"{name} is already registered.")
+        if key in self._memory and not overwrite:
+            raise ValueError(f"{key} is already registered.")
 
-        self._data[name] = data
+        self._memory[key] = value
 
-    def unregister(self, name: str):
+    def unregister(self, key: str):
         """Unregister an entry."""
         if not self._case_sensitive:
-            name = name.lower()
+            key = key.lower()
 
-        self._data.pop(name, None)
+        self._memory.pop(key, None)
 
-    def get(self, name: str, default: typing.Any = None) -> typing.Any:
+    def get(self, key: str, default: typing.Any = None) -> typing.Any:
         """Get a registered entry."""
         if not self._case_sensitive:
-            name = name.lower()
+            key = key.lower()
 
-        return self._data.get(name, default)
+        return self._memory.get(key, default)
 
     @property
     def data(self) -> typing.Dict[str, typing.Any]:
-        """Return the registry data."""
-        return self._data
+        """Return the registry memory."""
+        warnings.warn(
+            "The 'data' property is deprecated, use 'memory' instead.",
+            DeprecationWarning,
+        )
+        return self._memory
+
+    @property
+    def keys(self) -> typing.List[str]:
+        """Return the registry keys."""
+        return list(self._memory.keys())
+
+    @property
+    def values(self) -> typing.List[typing.Any]:
+        """Return the registry values."""
+        return list(self._memory.values())
+
+    @property
+    def memory(self) -> typing.Dict[str, typing.Any]:
+        """Return the registry memory."""
+        return self._memory
 
     def __itter__(self):
-        return iter(self._data)
+        return iter(self._memory)
 
-    def __contains__(self, name: str) -> bool:
+    def __contains__(self, key: str) -> bool:
         """Check if an entry is registered."""
         if not self._case_sensitive:
-            name = name.lower()
+            key = key.lower()
 
-        return name in self._data
+        return key in self._memory
