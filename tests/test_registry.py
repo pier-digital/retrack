d725568bca9af97e3d6fd9264fe32a86a426c09a
Gabriel Guarisa <gabrielguarisa@gmail.com>
2024-10-18 11:01:38 -0300

    Fix registry test

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -10,30 +10,30 @@ def test_create_registry():
 
 def test_register_model():
     registry = Registry()
-    registry.register("test", str)
-    assert registry.data == {"test": str}
+    registry.register("test", "example")
+    assert registry.data == {"test": "example"}
 
 
 def test_get_model():
     registry = Registry()
-    registry.register("test", str)
-    assert registry.get("test") == str
+    registry.register("test", "example")
+    assert registry.get("test") == "example"
 
 
 def test_get_model_not_found():
     registry = Registry()
-    registry.register("test", str)
+    registry.register("test", "example")
     assert registry.get("test2") is None
 
 
 def test_contains_model():
     registry = Registry()
-    registry.register("test", str)
+    registry.register("test", "example")
     assert "test" in registry
 
 
 def test_model_already_registered():
     registry = Registry()
-    registry.register("test", str)
+    registry.register("test", "example")
     with pytest.raises(ValueError):
-        registry.register("test", str)
+        registry.register("test", "example")
