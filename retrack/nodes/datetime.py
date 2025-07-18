88d24df3c7b76c155f59f7357b13aa289abb642f
Pedro Sampaio <pedro.sampaio@pier.digital>
2025-07-18 20:11:09 -0300

    Add DaysBetweenDates Node

--- retrack/nodes/datetime.py
+++ retrack/nodes/datetime.py
@@ -33,3 +33,49 @@ class CurrentYear(BaseNode):
         input_void: typing.Optional[pd.Series] = None,
     ) -> typing.Dict[str, str]:
         return {"output_value": dt.datetime.now().year}
+
+
+###############################################################
+# DaysBetweenDates Inputs and Outputs
+###############################################################
+
+
+class DaysBetweenDatesOutputsModel(pydantic.BaseModel):
+    output_value: OutputConnectionModel
+
+
+class DaysBetweenDatesInputsModel(pydantic.BaseModel):
+    input_value_0: InputConnectionModel
+    input_value_1: typing.Optional[InputConnectionModel] = None
+
+
+class DaysBetweenDatesMetadataModel(pydantic.BaseModel):
+    format: str = "%Y-%m-%d"
+
+
+###############################################################
+# DaysBetweenDates Node
+###############################################################
+
+
+class DaysBetweenDates(BaseNode):
+    inputs: DaysBetweenDatesInputsModel
+    outputs: DaysBetweenDatesOutputsModel
+    data: DaysBetweenDatesMetadataModel
+
+    def run(
+        self,
+        input_value_0: pd.Series,
+        input_value_1: pd.Series,
+    ) -> typing.Dict[str, pd.Series]:
+        format = self.data.format
+        datetime_0 = dt.datetime.strptime(input_value_0.squeeze(), format)
+        datetime_1 = (
+            dt.datetime.now()
+            if input_value_1.squeeze() is None
+            else dt.datetime.strptime(input_value_1.squeeze(), format)
+        )
+
+        datetime_diff = abs((datetime_0 - datetime_1).days)
+
+        return {"output_value": datetime_diff}
--- tests/test_nodes/test_datetime.py
+++ tests/test_nodes/test_datetime.py
@@ -1,6 +1,9 @@
 import pytest
 
-from retrack.nodes.datetime import CurrentYear
+import pandas as pd
+import datetime as dt
+
+from retrack.nodes.datetime import CurrentYear, DaysBetweenDates
 
 
 @pytest.fixture
@@ -29,3 +32,57 @@ def test_current_year_node_run(current_year_input_data, mocker):
     current_year_node = CurrentYear(**current_year_input_data)
     output = current_year_node.run()
     assert output["output_value"] == 2021
+
+
+@pytest.fixture
+def days_between_dates_input_data():
+    return {
+        "id": 9,
+        "data": {},
+        "inputs": {
+            "input_value_0": {"connections": []},
+            "input_value_1": {"connections": []},
+        },
+        "outputs": {"output_value": {"connections": []}},
+        "position": [1597.1904571362154, 628.6495284260166],
+        "name": "DaysBetweenDates",
+    }
+
+
+def test_days_between_dates_node(days_between_dates_input_data):
+    days_between_dates_node = DaysBetweenDates(**days_between_dates_input_data)
+
+    assert isinstance(days_between_dates_node, DaysBetweenDates)
+
+    assert days_between_dates_node.model_dump(by_alias=True) == {
+        "id": "9",
+        "name": "DaysBetweenDates",
+        "data": {"format": "%Y-%m-%d"},
+        "inputs": {
+            "input_value_0": {"connections": []},
+            "input_value_1": {"connections": []},
+        },
+        "outputs": {"output_value": {"connections": []}},
+    }
+
+
+def test_days_between_dates_node_run(days_between_dates_input_data, mocker):
+    days_between_dates_node = DaysBetweenDates(**days_between_dates_input_data)
+    date_time_now = dt.datetime.strptime("2025-07-01", "%Y-%m-%d")
+
+    class FixedDateTime(dt.datetime):
+        @classmethod
+        def now(cls, tz=None):
+            return date_time_now
+
+    output = days_between_dates_node.run(
+        pd.Series(["2025-07-01"]), pd.Series(["2025-01-01"])
+    )
+    assert (output["output_value"] == pd.Series([181])).all()
+    mocker.patch("retrack.nodes.datetime.dt.datetime", FixedDateTime)
+    output = days_between_dates_node.run(pd.Series(["2025-07-01"]), pd.Series([None]))
+    assert (output["output_value"] == pd.Series([0])).all()
+    output = days_between_dates_node.run(pd.Series(["2025-01-01"]), pd.Series([None]))
+    assert (output["output_value"] == pd.Series([181])).all()
+    output = days_between_dates_node.run(pd.Series(["2024-12-31"]), pd.Series([None]))
+    assert (output["output_value"] == pd.Series([182])).all()
